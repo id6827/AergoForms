ca1d9cf7cd584583ed87281c02cf74dafffb17ff
sunga <sunga@blocko.io>
2020-01-06 17:34:20 +0900

    Add drilldown graph
    Signed-off-by: sunga <sunga@blocko.io>

--- src/main/resources/public/js/chart.js
+++ src/main/resources/public/js/chart.js
@@ -5,10 +5,58 @@ var data = new Array();
 for(var i=0; i<answers.length; i++){
 	data.push({
 		name: answers[i].text,
-		y: answers[i].count,
-		drilldown: null
+		y: answers[i].count / total * 100,
+		count: answers[i].count,
+		drilldown: answers[i].text
 	});
 }
+console.log(data);
+var series = new Array();
+for(var i=0; i<answers.length; i++){
+	series.push({
+		name: answers[i].text,
+		id: answers[i].text,
+		data: [
+			{
+				name: "00:00",
+				y: 5.6,
+				count: 1
+			},
+			{
+				name: "04:00",
+				y: 3.2,
+				count: 1
+			},
+			{
+				name: "08:00",
+				y: 4.4,
+				count: 1
+			},
+			{
+				name: "12:00",
+				y: 6.8,
+				count: 1
+			},
+			{
+				name: "16:00",
+				y: 32.0,
+				count: 1
+			},
+			{
+				name: "20:00",
+				y: 25.0,
+				count: 1
+			},
+			{
+				name: "24:00",
+				y: 26.0,
+				count: 1
+			}
+        ]
+	});
+}
+console.log(series);
+
 
 //console.log(data)
 // Create the chart
@@ -32,9 +80,9 @@ Highcharts.chart('container', {
     },
     yAxis: {
     		min: 0,
-    		max: total,
+    		max: 100,
         title: {
-            text: 'Total'
+            text: 'Total percent'
         }
 
     },
@@ -46,14 +94,14 @@ Highcharts.chart('container', {
             borderWidth: 0,
             dataLabels: {
                 enabled: true,
-                format: '{point.y:.1f}'
+                format: '{point.y:.0f}%'
             }
         }
     },
 
     tooltip: {
         headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> of ' + total + '.00 <br/>'
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.count:.0f}</b> of ' + total + '<br/>'
     },
 
     series: [
@@ -64,7 +112,6 @@ Highcharts.chart('container', {
         }
     ],
     drilldown: {
-        series: null
+    		series: series
     }
 });
-
